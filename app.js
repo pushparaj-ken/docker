d1602ac5a8c64f2c09cf4e6c01e95faea6247a42
pushparaj-ken <policepushparaj96@gmail.com>
2024-05-03 11:25:25 +0530

    completed

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const dotenv = require('dotenv')
 dotenv.config();
+console.log("TCL: ", process.env.port)
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
